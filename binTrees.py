e74873d9122bd76e39921ed71d36b5f98f03936d
Nikolay Sidorov <nikolaysidorov@MacBook-Air-von-Nikolay.local>
2025-04-18 15:45:22 +0200

    added bin trees

---
+++ binTrees.py
@@ -0,0 +1,52 @@
+import math
+
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+def printTree(tree:TreeNode, message="root"):
+    print(f"{message} {tree.key}")
+    if(tree.left != None):
+        printTree(tree.left, "left")
+    if(tree.right!=None):
+        printTree(tree.right, "right")
+
+
+node0 = TreeNode(2)
+node1 = TreeNode(3)
+node2 = TreeNode(5)
+node3 = TreeNode(1)
+node4 = TreeNode(3)
+node5 = TreeNode(7)
+node6=TreeNode(4)
+node7=TreeNode(6)
+node8=TreeNode(8)
+node0.left=node1
+node0.right=node2
+node1.left=node3
+node2.left=node4
+node2.right=node5
+node4.right=node6
+node5.left=node7
+node5.right=node8
+
+tree = node0
+printTree(tree)
+
+tree_tuple = ((1,3,None),2,((None,3,4),5,(6,7,8)))
+
+def parse_tree(data):
+    if (isinstance(data,tuple) and len(data==3)):
+        node = TreeNode(data[1])
+        node.left=parse_tree(data[0])
+        node.right=parse_tree(data[1])
+    elif (data==None):
+        node=None
+    else:
+        node = TreeNode(data)
+    return node
+
+tree2 = parse_tree(tree_tuple)
+printTree(tree2)
--- sorts.py
+++ sorts.py
@@ -1,1 +1,16 @@
 import math
+
+test = [0,3,2,4,7,5,9,1]
+
+def bubble_sort(list:list,i):
+    for i in range(i, 0, -1):
+        for j in range(0, i-1):
+            if(list[j]>list[j+1]):
+                list[j], list[j+1]=list[j+1],list[j]
+            print(f"{i}: {list}")
+    return list
+def insertion_sort(list:list):
+    pass
+
+bubres = bubble_sort(test, len(test))
+print(bubres)
